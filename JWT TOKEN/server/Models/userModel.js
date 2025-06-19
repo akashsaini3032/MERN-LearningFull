dd9512c726c8fa028b4a92524a30b34ae6527b06
akashsaini3032 <akashsainira@gmail.com>
2025-06-19 22:50:07 +0530

    update

--- JWT TOKEN/server/Models/userModel.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-const usrSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String,
-});
-module.exports = mongoose.model("usr", usrSchema);
--- JWT TOKEN/server/Routes/userRoute.js
+++
@@ -1,8 +0,0 @@
-const express= require("express")
-const route=express.Router()
-const usrController=require("../Controllers/userController")
-route.post("/signup",usrController.usrSignup)
-route.post("/login",usrController.usrLogin)
-route.post("/auth",usrController.usrAuthentication)
-
-module.exports=route
