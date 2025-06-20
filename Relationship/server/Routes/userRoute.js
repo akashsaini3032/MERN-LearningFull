2acaca6d363f8c000d51258e69894c1b1c4029db
akashsaini3032 <akashsainira@gmail.com>
2025-06-20 22:55:25 +0530

    update

--- Relationship/server/Routes/userRoute.js
+++
@@ -1,6 +0,0 @@
-const express= require("express")
-const route=express.Router()
-const usrController=require("../Controllers/userController")
-route.post("/usersave",usrController.userSave)
-
-module.exports=route
