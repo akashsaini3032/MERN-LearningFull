e91a841e025ed6fe34362f90a2c5189c919d1bd9
akashsaini3032 <akashsainira@gmail.com>
2025-06-28 20:47:16 +0530

    folder update

--- STRIPE~PaymentGateway/server/routes/paymentRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const paymentController = require('../controllers/paymentController');
-router.post('/create-payment-intent', paymentController.createPaymentIntent);
-// router.post('/webhook', express.raw({ type: 'application/json' }), paymentController.handleWebhookEvents);
-
-module.exports = router;
