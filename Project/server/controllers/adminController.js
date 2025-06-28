52863f72d70dc20904502a21f11b25f01a4d8fd6
akashsaini3032 <akashsainira@gmail.com>
2025-06-28 23:17:47 +0530

    project backend

---
+++ Project/server/controllers/adminController.js
@@ -0,0 +1,25 @@
+const AdminModel= require("../models/adminModel");
+
+
+
+const adminLogin=async(req, res)=>{
+    const { adminid , password } = req.body;  
+    const Admin= await AdminModel.findOne({adminid:adminid});
+    if (!Admin)
+    {
+        res.status(401).send({msg:"Invalid Admin ID"});
+    }
+
+    if (Admin.password!=password)
+    {
+         res.status(401).send({msg:"Invalid Credentials!"});
+    }
+
+  
+    res.status(201).send(Admin);
+
+}
+
+module.exports={
+    adminLogin
+}
