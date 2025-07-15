215b1c6f562232f296a47a6d54c17e5a30819ed0
akashsaini3032 <akashsainira@gmail.com>
2025-07-16 00:27:28 +0530

    project work

--- Demo2/server/models/userModel.js
+++ Demo2/server/models/userModel.js
@@ -1,10 +1,34 @@
-const mongoose= require("mongoose");
-const userSchema= new mongoose.Schema({
-     name:String,
-     city:String,
-     address:String,
-     pincode:Number,
-     email:String,
-     password:String
-})
+// const mongoose= require("mongoose");
+// const userSchema= new mongoose.Schema({
+//      name:String,
+//      city:String,
+//      address:String,
+//      pincode:Number,
+//      email:String,
+//      password:String
+// })
+// module.exports = mongoose.model("user", userSchema);
+
+
+
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    city: String,
+    address: String,
+    pincode: Number,
+    email: String,
+    password: String,
+
+    // ✅ Google login extra fields (Non-breaking changes)
+    fromGoogle: {
+        type: Boolean,
+        default: false
+    },
+    profilePic: {
+        type: String
+    }
+});
+
 module.exports = mongoose.model("user", userSchema);
