0d93a70b4ba0dd20b358a9bb12485f3fcaebf483
akashsaini3032 <akashsainira@gmail.com>
2025-09-05 20:44:42 +0530

    practice

---
+++ PRACTICE/React/src/Cybrom.jsx
@@ -0,0 +1,14 @@
+
+
+const Cybrom=()=>{
+    return(
+        <>
+
+        <h1 className="akash">This is Cybrom file</h1>
+        
+        </>
+    )
+}
+
+
+export default Cybrom;
---
+++ PRACTICE/React/src/Data.jsx
@@ -0,0 +1,12 @@
+
+
+const Data=()=>{
+    return(
+        <>
+        
+        <h1 id="cybrom">
+            this is Data Part </h1></>
+    )
+}
+
+export default Data;
