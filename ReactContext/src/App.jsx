457b021b1bcf41c7abd3e60d933f57ed20c72f52
akashsaini3032 <akashsainira@gmail.com>
2025-09-08 21:15:30 +0530

    practice

---
+++ ReactContext/src/App.jsx
@@ -0,0 +1,26 @@
+
+
+
+import { useState, createContext } from "react";
+import Comp1 from "./Comp1";
+const myContext=createContext();
+
+
+const App=()=>{
+    const [user, setUser]= useState("Akash");
+    return(
+        <>
+        <h1>Welcome {user} !!!</h1>
+        <button onClick={()=>{setUser("Saini")}}>
+            click here
+        </button>
+        <myContext.Provider value={{user,setUser}}>
+            <Comp1 />
+        </myContext.Provider>
+      
+        
+        </>
+    )
+}
+export default App;
+export {myContext};
