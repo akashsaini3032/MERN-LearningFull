937b22b097a9b554be950448b8e1254e9a1e3ec7
akashsaini3032 <akashsainira@gmail.com>
2025-09-18 21:24:09 +0530

    practice

--- MERN/backend/practice.js
+++ MERN/backend/practice.js
@@ -1,55 +1,66 @@
 
-const stuModels=require=("../Models/stuModels");
+
+
+const stuModels=require("../Models/stuModels");
+
 
 const homePage=(req,res)=>{
-    console.log("iam homepage fnc");
+    console.log("akash");
     res.send("ok");
 };
 
 const studentSave=(req,res)=>{
     const {rollno,name,city,fees}=req.body;
+
     const student=stuModels.create({
         rollno:rollno,
         name:name,
         city:city,
         fees:fees,
     });
-    res.send({msg:"aksh",myData:student});
-};
+
+    res.send({msg:"akash"},myData:student);
+}
+
 
 const dataDisplay=async(req,res)=>{
     const Data=await stuModels.find();
     res.send(Data);
 };
 
-
-const updateShow=async(req,res)=>{
+const updateshow=async(req,res)=>{
     const Data=await stuModels.find();
     res.send(Data);
 };
 
-
 const dataDelete=async(req,res)=>{
-    const Data=await stuModels.find();
-    res.send(data);
-};
+
+
+    const {myid}=req.query;
+
+    const Data=await stuModels.findByIdAndDelete(myid);
+    res.send({msg:"",Data:Data});
+}
+
+
 
 
 const editDataSave=async(req,res)=>{
-    const {_id,rollno,name,city,fees}=req.body;
-    const Data=await stuModels.findByIdAndUpdate(_id,{rollno,name,fees});
+    const {id,rollno,name,city,fees}=req.body;
+    const Data= await stuModels.findByIdAndDelete(id,{rollno,name,city,fees});
+    res.send({msg:"Data SucessFully Updated"});
 
-
-    res.send({msg:"Daya"});
 }
 
-const getSearchData=async(req,res)=>{
+
+const getSearhData=async(req,res)=.{
     const {rno}=req.query;
-    const Data=await stuModels.find({"rollno":rno})
+    const Data=await stuModels.find({"rollno":rno});
     res.send(Data);
 }
-
 
 module.exports={
     
 }
+
+
