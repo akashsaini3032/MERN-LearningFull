7113e4a4a3ee7f6524d036e5c7e7221b656aa34c
akashsaini3032 <akashsainira@gmail.com>
2025-09-21 19:40:07 +0530

    practice

---
+++ MERN/backend/practice.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+const mongoose=require("mongoose")
+
+const stuSchema=mongoose.Schema({
+    rollno:Number,
+    name:String,
+    city:String,
+    fees:Number
+})
+
+module.exports=mongoose.model("student",stuSchema)
+
+
+cost mongoose=require("mongoose")
+
+const stuSchema=mongoose.Schema({
+    rollno:Number,
+    name:String,
+    city:String,
+    fees:Number
+})
