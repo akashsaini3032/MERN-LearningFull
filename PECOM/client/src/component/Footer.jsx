0ea5cfeffd97c8f6501a2eb0619ca80d92a9d517
akashsaini3032 <akashsainira@gmail.com>
2025-09-21 19:43:07 +0530

    practice

---
+++ PECOM/client/src/component/Footer.jsx
@@ -0,0 +1,13 @@
+
+
+const Footer=()=>{
+    return(
+        <>
+         <div id="copyright">
+            www.myshopping.com all right reserved. 2025  
+          </div>        
+        </>
+    )
+}
+
+export default Footer;
