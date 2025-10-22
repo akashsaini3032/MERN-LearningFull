b059f7a688e84f9b547a8fc4dd3a93fabce8fcdb
akashsaini3032 <akashsainira@gmail.com>
2025-10-22 23:31:53 +0530

    middleware practice

--- middleware/client/src/App.jsx
+++ middleware/client/src/App.jsx
@@ -1,30 +1,30 @@
 
 
 
-// import React from 'react'
-// import { useState } from 'react'
-// import axios from 'axios'
+import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
 
 
-// const App = () => {
-//    const [name,setName]  = useState("jack");
+const App = () => {
+   const [name,setName]  = useState("jack");
    
 
-//     const sendData = async()=>{
-//       console.log("click");
+    const sendData = async()=>{
+      console.log("click");
       
-//         const req = await axios.post("http://localhost:3000/home",{Name:name});
-//          console.log(req.data);
-//          setName(req.data)
+        const req = await axios.post("http://localhost:3000/home",{Name:name});
+         console.log(req.data);
+         setName(req.data)
          
-//     }
+    }
 
-//   return (
-//     <div>
-//       <h1>{name}</h1>
-//       <button onClick={sendData}>send</button>
-//     </div>
-//   )
-// }
+  return (
+    <div>
+      <h1>{name}</h1>
+      <button onClick={sendData}>send</button>
+    </div>
+  )
+}
 
-// export default App
+export default App
