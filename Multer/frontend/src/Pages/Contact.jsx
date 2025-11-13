afe12e18c986bc6b7bef7859c2052e9d5fc01e26
akashsaini3032 <akashsainira@gmail.com>
2025-11-13 22:56:58 +0530

    practice

--- Multer/frontend/src/Pages/Contact.jsx
+++ Multer/frontend/src/Pages/Contact.jsx
@@ -1,8 +1,8 @@
-const Contact=()=>{
-    return(
-        <>
-        <h1>contact us</h1>
-        </>
-    )
-}
-export default Contact
+// const Contact=()=>{
+//     return(
+//         <>
+//         <h1>contact us</h1>
+//         </>
+//     )
+// }
+// export default Contact
