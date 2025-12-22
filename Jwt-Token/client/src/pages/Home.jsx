9f1d2899086464be5d18e0962c744a1408d9e571
akashsaini3032 <akashsainira@gmail.com>
2025-12-22 22:57:46 +0530

    practice

--- Jwt-Token/client/src/pages/Home.jsx
+++ Jwt-Token/client/src/pages/Home.jsx
@@ -1,28 +1,28 @@
-// import axios from "axios";
-// import { useState, useEffect } from "react";
-// import { useNavigate } from "react-router-dom";
-// const Home=()=>{
-//   const navigate=useNavigate();
-//   const userAuthentication=async()=>{
-//      const token=localStorage.getItem("token");
-//      if (!token)
-//      {
-//        navigate("/login");
-//      }
-//         let api="http://localhost:8000/user/authentication";
-//         const response = await axios.post(api, null, {headers: { "auth-token": token } });
-//         console.log(response.data);
-//          localStorage.setItem("username", response.data.name);
-//          localStorage.setItem("email", response.data.email);
-//          navigate("/dashboard");
-//   }
-//   useEffect(()=>{
-//     userAuthentication();
-//   }, [])
-//     return(
-//         <>
-//           <h1> Welcome To Home Page</h1>
-//         </>
-//     )
-// }
-// export default Home;
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+const Home=()=>{
+  const navigate=useNavigate();
+  const userAuthentication=async()=>{
+     const token=localStorage.getItem("token");
+     if (!token)
+     {
+       navigate("/login");
+     }
+        let api="http://localhost:8000/user/authentication";
+        const response = await axios.post(api, null, {headers: { "auth-token": token } });
+        console.log(response.data);
+         localStorage.setItem("username", response.data.name);
+         localStorage.setItem("email", response.data.email);
+         navigate("/dashboard");
+  }
+  useEffect(()=>{
+    userAuthentication();
+  }, [])
+    return(
+        <>
+          <h1> Welcome To Home Page</h1>
+        </>
+    )
+}
+export default Home;
