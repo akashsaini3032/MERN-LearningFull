0f87a1e7145ed8433da790bf5f6290394e2dac09
akashsaini3032 <akashsainira@gmail.com>
2026-01-05 22:43:24 +0530

    jwt practice

--- Jwt-Token/server/models/userModel.js
+++ Jwt-Token/server/models/userModel.js
@@ -1,17 +1,17 @@
-const mongoose= require("mongoose");
-const userSchema= new mongoose.Schema({
-     name: {
-        type: String,
-        require: true
-    },
-    email:{
-        type:String,
-        require: true
-    },
-    password:{
-        type:String,
-        require: true
-    },
-    createdAt: { type: Date, default: Date.now }
-})
-module.exports = mongoose.model("user", userSchema);
+// const mongoose= require("mongoose");
+// const userSchema= new mongoose.Schema({
+//      name: {
+//         type: String,
+//         require: true
+//     },
+//     email:{
+//         type:String,
+//         require: true
+//     },
+//     password:{
+//         type:String,
+//         require: true
+//     },
+//     createdAt: { type: Date, default: Date.now }
+// })
+// module.exports = mongoose.model("user", userSchema);
