fd1b1c53aedbb085716d5174fc42df3f8702f652
akashsaini3032 <akashsainira@gmail.com>
2026-01-06 23:03:55 +0530

    mern practice

--- Task-Management1/client/src/Layout.jsx
+++ Task-Management1/client/src/Layout.jsx
@@ -1,21 +1,21 @@
-// import { Outlet, Link } from "react-router-dom";
-// import Header from "./components/Header";
-// import Footer from "./components/Footer";
+import { Outlet, Link } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 
-// const Layout = () => {
+const Layout = () => {
 
-//     return(
-//         <>
-//         <Header/>
-//        <div id="container">
-//          <Outlet/>
-//        </div>
-//         <Footer/>
+    return(
+        <>
+        <Header/>
+       <div id="container">
+         <Outlet/>
+       </div>
+        <Footer/>
         
-//         </>
-//     )
-// }
+        </>
+    )
+}
 
 
-// export default Layout;
+export default Layout;
