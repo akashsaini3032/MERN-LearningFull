e17c597cba6437b843b706d2958326317e60d9a1
akashsaini3032 <akashsainira@gmail.com>
2026-02-04 22:39:27 +0530

    cloudinary practice

--- Cloudary/backend/route/stuRoutes.js
+++ Cloudary/backend/route/stuRoutes.js
@@ -27,16 +27,16 @@
 // module.exports= route;
 
 
-const express=require("express")
-const route=express.Router()
-const stuController=require("../Controllers/stuControllers")
-route.get("/home",stuController.homePage)
-route.get("/display",stuController.displayData)
-route.get("/search",stuController.searchData)
-route.post("/save",stuController.studentSave)
-route.get("/update",stuController.updateShow)
-route.delete("/datadelete",stuController.dataDelete)
-route.get("/edit",stuController.editdataShow);
-route.put("/editdatasave",stuController.editDataSave);
+// const express=require("express")
+// const route=express.Router()
+// const stuController=require("../Controllers/stuControllers")
+// route.get("/home",stuController.homePage)
+// route.get("/display",stuController.displayData)
+// route.get("/search",stuController.searchData)
+// route.post("/save",stuController.studentSave)
+// route.get("/update",stuController.updateShow)
+// route.delete("/datadelete",stuController.dataDelete)
+// route.get("/edit",stuController.editdataShow);
+// route.put("/editdatasave",stuController.editDataSave);
 
-module.exports= route
+// module.exports= route
