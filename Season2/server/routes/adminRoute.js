e545b614efe0e6de7f98000339af4ef5cedc1e8f
akashsaini3032 <akashsainira@gmail.com>
2026-02-09 22:33:24 +0530

    season practice

--- Season2/server/routes/adminRoute.js
+++ Season2/server/routes/adminRoute.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const route= express.Router();
-const AdminController= require("../controllers/adminController");
+// const express = require("express");
+// const route= express.Router();
+// const AdminController= require("../controllers/adminController");
 
 
-route.post("/adminlogin", AdminController.adminLogin);
-route.post("/productsave", AdminController.productSave);
+// route.post("/adminlogin", AdminController.adminLogin);
+// route.post("/productsave", AdminController.productSave);
 
 
-module.exports=route;
+// module.exports=route;
