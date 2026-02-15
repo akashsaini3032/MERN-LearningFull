6a2d7ecccd0299e609ee5e0f89ecb64b31facdd5
akashsaini3032 <akashsainira@gmail.com>
2026-02-15 22:38:01 +0530

    mern update

--- Jwt-Token/client/src/pages/DashBoard.jsx
+++ Jwt-Token/client/src/pages/DashBoard.jsx
@@ -1,18 +1,18 @@
-import { useNavigate } from "react-router-dom";
-const DashBoard=()=>{
-const navigate = useNavigate();
- const logout=()=>{
-    localStorage.clear();
-    navigate("/");
+// import { useNavigate } from "react-router-dom";
+// const DashBoard=()=>{
+// const navigate = useNavigate();
+//  const logout=()=>{
+//     localStorage.clear();
+//     navigate("/");
      
- }
-    return(
-        <>
-          <h1> Welcome To User Dashboard</h1>
-          Welcome {localStorage.getItem("username") } ! 
-          <a href="#" onClick={logout}>Logout</a>
-        </>
-    )
-}
+//  }
+//     return(
+//         <>
+//           <h1> Welcome To User Dashboard</h1>
+//           Welcome {localStorage.getItem("username") } ! 
+//           <a href="#" onClick={logout}>Logout</a>
+//         </>
+//     )
+// }
 
-export default DashBoard;
+// export default DashBoard;
